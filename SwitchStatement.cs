577f012d26a8b38bd8ab9407a79828f365b4f7e2
Naimul Islam <naimul3070@hotmail.com>
2021-03-08 16:14:43 +0600

    C#OOP

---
+++ SwitchStatement.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Naimul
+//{
+//    class SwitchStatement
+//    {
+//        static void Main(string[] args)
+//        {
+//            int day = 4;
+
+//            switch (day)
+//            {
+//                case 1:
+//                    Console.WriteLine("Monday");
+//                    break;
+//                case 2:
+//                    Console.WriteLine("Tuesday");
+//                    break;
+//                case 3:
+//                    Console.WriteLine("Wednesday");
+//                    break;
+//                case 4:
+//                    Console.WriteLine("Thursday");
+//                    break;
+
+//                    Console.ReadKey();
+//            }
+
+//        }
+//    }
+//}
---
+++ TernaryNestedTernaryOperator.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Naimul
+//{
+//    class TernaryNestedTernaryOperator
+//    {
+//        static void Main(string[] args)
+//        {
+//            int no1 = 10;
+//            int no2 = 20;
+
+//            //if (no1 > no2)
+//            //{
+//            //    Console.WriteLine(no1 + no2);
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine(no2 - no1);
+//            //}
+
+//            //other way
+
+//            int result = (no1 > no2) ? (no1 + no2): (no1 - no1);
+//            Console.WriteLine(result);
+
+//            Console.ReadKey();
+
+//        }
+//    }
+//}
