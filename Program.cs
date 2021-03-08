961c30ef8c0bf0627a12fed32cef6e4cf9b32339
Naimul Islam <naimul3070@hotmail.com>
2021-03-08 14:20:36 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+
+//namespace Naimul
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //implicit custing
+
+//            int myNum = 9;
+//            double myDouble = myNum;
+
+//            Console.WriteLine(myNum + "\n");
+//            Console.WriteLine(myDouble + "\n");
+
+
+//            //explicit custing
+
+//            double another = 9.75;
+//            int anotherInt = (int)another;
+
+//            Console.WriteLine(another + "\n");
+//            Console.WriteLine(anotherInt + "\n");
+//        }
+//    }
+//}
---
+++ UserInput.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Naimul
+//{
+//    class UserInput
+//    {
+
+//        static void Main(String[] args)
+//        {
+//            string name;
+//            int age;
+//            double balance;
+//            char ch;
+
+//            name = Console.ReadLine(); 
+//            age = Convert.ToInt32(Console.ReadLine());
+//            balance = Convert.ToDouble(Console.ReadLine());
+//            ch = Convert.ToChar(Console.ReadLine());
+
+
+//            Console.WriteLine("Name is : " + name);
+//            Console.WriteLine("Age is : " + age);
+//            Console.WriteLine("Balance is is : " + balance);
+//            Console.WriteLine("character is : " + ch);
+
+//            Console.ReadKey();
+//        }
+//    }
+   
+//}
