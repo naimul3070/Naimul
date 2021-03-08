847ba06405eccecd4db5bbdf778ced4df416b7dd
Naimul Islam <naimul3070@hotmail.com>
2021-03-08 15:49:17 +0600

    if else

---
+++ ifelseelseif.cs
@@ -0,0 +1,28 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Naimul
+//{
+//    class ifelseelseif
+//    {
+//        static void Main(string[] args)
+//        {
+//            int time = 22;
+//            if (time < 10)
+//            {
+//                Console.WriteLine("Good morning");
+//            }
+//            else if (time < 20)
+//            {
+//                Console.WriteLine("Good Day");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Good Evening");
+//            }
+
+//            Console.ReadKey();
+//        }
+//    }
+//}
