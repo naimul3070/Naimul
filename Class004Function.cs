accd42b66a4f4212e08b69258f0dcdb52a26f7b7
Naimul Islam <naimul3070@hotmail.com>
2021-03-16 15:08:02 +0600

    class4 EF2

---
+++ Class004Function.cs
@@ -0,0 +1,28 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Naimul
+//{
+//    class Class004Function
+//    {
+//        static void Main(string[] args)
+//        {
+//            int a;
+
+           
+//           arif2(out a);
+
+//            Console.WriteLine("A : {0}", a);
+//            Console.ReadKey();
+//        }
+
+//        static void arif2(out int b)
+//        {
+//            b = 0;
+//            b = b + 50;
+
+//            Console.WriteLine("F : {0}", b);
+//        }
+//    }
+//}
