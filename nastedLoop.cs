a220fe1c997a3359d6a4c0460f2b1f2e53a2eb99
Naimul Islam <naimul3070@hotmail.com>
2021-03-22 18:15:31 +0600

    @@

--- nastedLoop.cs
+++ nastedLoop.cs
@@ -1,28 +1,28 @@
-﻿////using System;
-////using System.Collections.Generic;
-////using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-////namespace Naimul
-////{
-////    class nastedLoop
-////    {
-////        static void Main(string[] args)
-////        {
-            
-////            int height = 12;/*int.Parse(Console.ReadLine());*/
-////            int tmp = 0;
-////            for (int i = 1; i <= (height * 2) - 1 ; i++)
-////            {
-////                if (i > height)
-////                {
-////                    tmp += 1;
-////                }
-////                for (int j = 1; j <= i-(tmp*2); j++)
-////                {
-////                    Console.Write("*");
-////                }
-////                Console.WriteLine();
-////            }
-////        }
-////    }
-////}
+//namespace Naimul
+//{
+//    class nastedLoop
+//    {
+//        static void Main(string[] args)
+//        {
+
+//            int height = 12;/*int.Parse(Console.ReadLine());*/
+//            int tmp = 0;
+//            for (int i = 1; i <= (height * 2) - 1; i++)
+//            {
+//                if (i > height)
+//                {
+//                    tmp += 1;
+//                }
+//                for (int j = 1; j <= i - (tmp * 2); j++)
+//                {
+//                    Console.Write("*");
+//                }
+//                Console.WriteLine();
+//            }
+//        }
+//    }
+//}
